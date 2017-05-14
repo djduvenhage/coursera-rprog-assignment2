5b4ef528626cad747be2d1a3e0390bb44ee47965
djduvenhage <duvenhage@gmx.net>
2017-05-14 15:01:50 -0600

    R Assignment2
    Inverse of Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,61 +1,91 @@
-# Matrix inversion is usually a costly computation and there may be some benefit
-# to caching the inverse of a matrix rather than compute it repeatedly. The
-# following two functions are used to cache the inverse of a matrix.
+# May 14th, 2017
+# Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse 
+# of a matrix rather than compute it repeatedly. The following two functions are used to cache the inverse 
+# of a matrix.
 
-# makeCacheMatrix creates a list containing a function to
-# 1. set the value of the matrix
-# 2. get the value of the matrix
-# 3. set the value of inverse of the matrix
-# 4. get the value of inverse of the matrix
+# makeCacheMatrix function creates a list containing a function to
+# 1. set value of matrix
+# 2. get value of matrix
+# 3. set value of inverse of matrix
+# 4. get value of inverse of matrix
+
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    setinverse <- function(inverse) inv <<- inverse
-    getinverse <- function() inv
-    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+        invs <- NULL
+        set <- function(y) {
+                x <<- y
+                invs <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) invs <<- inverse
+        getinverse <- function() invs
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
+# cachemean function returns the inverse of a matrix. It first checks if the inverse has already been 
+# computed. If so, the function gets the result and skips the inversion calculation. If not valid, it 
+# calculates the inverse of the matrix, and sets the value in the cache using the setinverse function.
 
-# The following function returns the inverse of the matrix. It first checks if
-# the inverse has already been computed. If so, it gets the result and skips the
-# computation. If not, it computes the inverse, sets the value in the cache via
-# setinverse function.
+# function assumes the matrix is always invertible
 
-# This function assumes that the matrix is always invertible.
 cacheSolve <- function(x, ...) {
-    inv <- x$getinverse()
-    if(!is.null(inv)) {
-        message("getting cached data.")
-        return(inv)
-    }
-    data <- x$get()
-    inv <- solve(data)
-    x$setinverse(inv)
-    inv
+        invs <- x$getinverse()
+        if(!is.null(invs)) {
+                message("getting cached data")
+                return(invs)
+        }
+        data <- x$get()
+        invs <- solve(data)
+        x$setinverse(invs)
+        invs
 }
 
-## Sample run:
-## > x = rbind(c(1, -1/4), c(-1/4, 1))
-## > m = makeCacheMatrix(x)
-## > m$get()
-##       [,1]  [,2]
-## [1,]  1.00 -0.25
-## [2,] -0.25  1.00
 
-## No cache in the first run
-## > cacheSolve(m)
-##           [,1]      [,2]
-## [1,] 1.0666667 0.2666667
-## [2,] 0.2666667 1.0666667
+# Solution by theory:
 
-## Retrieving from the cache in the second run
-## > cacheSolve(m)
-## getting cached data.
-##           [,1]      [,2]
-## [1,] 1.0666667 0.2666667
-## [2,] 0.2666667 1.0666667
-## > 
+#Find the inverse of the matrix A = ( 3 1
+#                                     4 1)
+#
+#                       1            (2  -1
+# Formula A^-1 = _________________ * 
+#               ((3)(2) - (1)(4))    -4  3)
+#
+#                  1      (2  -1)           (1  -0.5
+#              =  ___  *              0r                 as per example below on "cacheSolve(invs)" call
+#                  2      -4   3)           -2   1.5 ) 
+
+
+
+# Solution by cacheSolve(invs) function:
+  
+#  > x <- matrix(c(3,4,1,2),2,2)
+#  >  invs <- makeCacheMatrix(x)
+#  >  invs$get()
+#           [,1] [,2]
+#     [1,]    3    1
+#     [2,]    4    2
+#
+#  > cacheSolve(invs)
+#           [,1] [,2]
+#     [1,]    1 -0.5
+#     [2,]   -2  1.5
+#  
+#  > cacheSolve(invs)#
+#    getting cached data
+#           [,1] [,2]
+#     [1,]    1 -0.5
+#     [2,]   -2  1.5 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
